8f66826d977fef3559861154efcc851ea6cabf0e
Ishikaaa <39964543+Ishikaaa@users.noreply.github.com>
2020-08-10 11:45:08 +0530

    Validate Sub Sequence

--- 100 Days Code/Day_005.py
+++ 100 Days Code/Day_005.py
@@ -1,5 +1,5 @@
 """
-Question: Validate Sub sequence
+Question: Validate Sub Sequence
 
 Given two non-empty arrays of integers, write a function that determines whether the second array is a subsequence of the first one.
 
