53c6a6d441c532f1f15fd9d651413b7a209b0350
Ishikaaa <39964543+Ishikaaa@users.noreply.github.com>
2020-08-14 18:55:45 +0530

    Keys And Rooms

--- 100 Days Code/Day_026.py
+++ 100 Days Code/Day_026.py
@@ -57,3 +57,24 @@ if __name__ == "__main__":
   print(canVisitAllRooms(rooms))
   
 
+# Solution - 2
+def canVisitAllRooms(rooms):
+  key_stack = [0]
+  rooms_visited = [0]*len(rooms)
+  rooms_visited[0]=1
+  while key_stack:
+    k = key_stack.pop()
+    if rooms_visited[k] == 0:
+      rooms_visited[k]=1
+    for new_key in rooms[k]:
+      if rooms_visited[new_key]==0:
+        key_stack.append(new_key)
+        #stack.extend(rooms[k])
+  for flag in rooms_visited:
+    if flag==0:
+      return False
+  return True
+
+if __name__ == "__main__":
+  rooms = [[1],[2],[3],[]]
+  print(canVisitAllRooms(rooms))
